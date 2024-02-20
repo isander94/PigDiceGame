c435ea43e06361f5cb7ba42b87619a5e35b79e2f
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-02-20 15:37:19 +0100

    Added documentation

--- player.py
+++ player.py
@@ -3,9 +3,10 @@ import dice
 
 class Player:
     def __init__(self, name):
+        """Creates player class, user needs to input their name"""
         self.name = name
 
     def change_name(self, new_name):
+        """Allows the user to change their name"""
         self.name = new_name
 
-    
