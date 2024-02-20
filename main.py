440cf3f7f25570cbf427805a057a83f33ed532aa
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-02-20 15:07:37 +0100

    Tested a gameloop

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Test")
-    print("Testing again")
+import cmd
 
-if __name__ == "__main__":
-    main()
+class GameLoop(cmd.Cmd):
+    intro = "=== Pig Dice Game ==="    
+    prompt = "--> "
+    
+    def do_quit(self, arg):
+        """Quits the program"""
+        return True
+    
+    def do_greet(self, arg):
+        """Simply prints hello to the screen"""
+        print("Hello, there!")
+
+    def do_showmenu(self, arg):
+        """Shows game menu"""
+        print("This is the menu of the game\nYou can choose to show the menu at any time")
+
+# Start the game loop
+GameLoop().cmdloop()
+
+if __name__ == '__main__':
+    GameLoop().cmdloop()
