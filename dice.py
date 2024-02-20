9d0f51c3f6e1c00a569ecefcc60095ac638f050d
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-02-20 15:29:52 +0100

    Added explanation of class and method

--- dice.py
+++ dice.py
@@ -2,10 +2,12 @@ import random
 
 class Dice:
     def __init__(self):
+        """Creates a dice class with low number 1 and high number 6, representing a 6-faced die"""
         self.low = 1
         self.high = 6
 
     def roll_dice(self):
+        """Rolls the dice for a number between 1 and 6"""
         random_num = random.randint(self.low, self.high)
         return random_num
     
