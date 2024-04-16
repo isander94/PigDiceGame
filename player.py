aec2f3269005ecb538d3bdde102dd3345900dddd
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-16 10:48:45 +0200

    added some functions

--- player.py
+++ player.py
@@ -1,12 +1,26 @@
 
-import dice
+from dice import Dice
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, score):
         """Creates player class, user needs to input their name"""
         self.name = name
+        self.score = score
 
     def change_name(self, new_name):
         """Allows the user to change their name"""
         self.name = new_name
 
+    def get_name(self):
+        """Gets the name of the player"""
+        return self.name
+    
+    def get_score(self):
+        """Gets the score of the player"""
+        return self.score
+    
+    def add_score(self,score):
+        """Adds to the score of the player"""
+        self.score += score
+
+        
