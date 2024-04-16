79ce9cbfd372acab29f15198040054b38e9493fe
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-16 12:03:25 +0200

    Added player rolling dice function

--- player.py
+++ player.py
@@ -23,4 +23,44 @@ class Player:
         """Adds to the score of the player"""
         self.score += score
 
+    def player_roll_dice(self):
+        """Player rolling the dice"""
+        turn_score = 0
+        while True:
+            nr_roll = Dice.roll_dice()
+            turn_score += nr_roll
+            total_score = turn_score + self.score
+
+            print(self.name + " rolled a " + nr_roll)
+
+            if total_score >= 25:
+                print(self.name + " wins!")
+                return self.get_score()
+
+
+            if nr_roll == 1:
+                print("Unlucky! You got a one! say bye bye to your points this round.")
+                turn_score = 0
+                break
+
+            if nr_roll > 1:
+                print(self.name + " Score this turn: " + turn_score )
+                print(self.name + " Total score: " + total_score )
+
+                decide = input("Roll again? (y/n): \n")
+                while decide.lower() != ("y" or "n"):
+                    decide = input("Error: Invalid input. Please only enter "'y'" or "'n'": ")
+
+                if decide.lower() == "y":
+                    continue
+                elif decide.lower() == "n":
+                    print(self.name + " Score this turn: " + turn_score )
+                    print(self.name + " Total score: " + total_score )
+                    self.add_score(turn_score)
+                    break
+
+            
+
+
+
         
