41af0daf5e2b87c9c518bdd8645a098c8c9392cd
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-04-16 16:57:25 +0200

    Added test checking for return

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import cmd
+import dice
+import player
+from game import Game
 
 class GameLoop(cmd.Cmd):
-    intro = "=== Pig Dice Game ==="   
+    intro = "=== Pig Dice Game ===\n1] Player VS Computer - pvc\n2] Player VS Player - pvp\n3] High score\n4]\n5]"
     prompt = "--> "
     
     def do_quit(self, line):
+
         """Quits the program"""
         return True
     
-    def do_greet(self, arg):
-        """Simply prints hello to the screen"""
-        print("Hello, there!")
-
-    def do_showmenu(self, arg):
+    def do_menu(self, arg):
         """Shows game menu"""
-        print("This is the menu of the game\nYou can choose to show the menu at any time")
+
+        print("\n1] Player VS Computer - PVC\n2] Player VS Player - PVP\n3] High score\n4]\n5]")
+
+    def do_pvc(self, arg):
+        """Selects the game mode to play against the computer"""
+
+        name = input("Enter name for player: ")
+        player = player.Player(name)
+
+
+    def do_pvp(self, arg):
+        """Selects the game mode for two players"""
+        print("Starting a player versus player game!")
+        name = input("Enter name of player 1: ")
+        player1 = player.Player(name)
+        name = input("Enter name of player 2: ")
+        player2 = player.Player(name)
+        pvp_game = Game(player1, player2)
+        pvp_game.start()
+
+    def do_highscore(self, arg):
+        """Show the high score of previous games by players"""
+        pass
+    def do_startgame(self, arg):
+        pass
+
+
 
 # Start the game loop
 
