f0df68cffed8d8377cdcbce6f330bafa541c3d79
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-16 15:34:43 +0200

    added a getter for the highscores

--- highscore.py
+++ highscore.py
@@ -4,14 +4,22 @@ class Highscore:
 
     def __init__(self):
         """creates highscore list"""
-        self.highscores = {}
+        self.highscore_list = {}
 
 
     def add_highscore(self, name, highscore):
         """Save a highscore"""
-        self.highscores[name] += highscore
+        self.highscore_list[name] += highscore
         with open("highscore.txt", "a") as file:
-            for player_name, score in self.highscores.items():
+            for player_name, score in self.highscore_list.items():
                 file.write(f"{player_name} - {score}\n")
 
+    def get_highscore(self):
+        """function to get names and their highscores"""
+        self.highscore_list = {}
+        with open("highscore.txt","r") as file:
+            for line in file:
+                name, score = line.strip().split("-")
+                self.highscore_list[name] = int(score)
+        return self.highscore_list
     
