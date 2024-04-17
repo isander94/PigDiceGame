a5e27a04435b726f5f9621847a9bfffd39fc1872
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-04-17 13:42:23 +0200

    Added win conditions

--- game.py
+++ game.py
@@ -13,56 +13,64 @@ class Game():
 
         # Main loop
         keep_going = True
-        while self.player1.get_score() < 20 or self.player2.get_score() < 20:
-            
+        while(keep_going):
+
             round_score = 0
-            print("NEW ROUND")
             # Player 1 turn
             print(f"\n--- {self.player1.get_name()}'s turn ---")
-            while keep_going:
-                
-                
+            while(keep_going):
                 # Ask the player to roll or hold
                 choice = input("Roll or Hold?\n--> ")
 
-                if choice.lower() == "roll":
+                if(choice.lower() == "roll"):
                     roll = self.player1.roll_dice()
-                    if roll == 1:
+                    if(roll == 1):
                         print(f"{self.player1.get_name()} rolled a {roll} and loses all points for this turn!")
                         round_score = 0
                         break
                     else:
                         round_score += roll
-                        print(f"{self.player1.get_name()} rolled a {roll} and now has {round_score} points!")
+                        print(f"{self.player1.get_name()} rolled a {roll} and now has {self.player1.get_score() + round_score} points!")
 
-                elif choice.lower() == "hold":
+                elif(choice.lower() == "hold"):
                     self.player1.add_score(round_score)
                     print(f"{self.player1.get_name()} has secured {self.player1.get_score()} points!")
                     round_score = 0
                     break
+            
+            # Check score
+            if(self.player1.get_score() >= 20):
+                print(f"{self.player1.get_name()} wins with a score of {self.player1.get_score()}!")
+                print("Game ended!")
+                break
 
+            # Player 2's turn
             print(f"\n--- {self.player2.get_name()}'s turn ---")
-            while keep_going:
-                # Player 2's turn
-                
+            while(keep_going):
                 # Ask the player to roll or hold
                 choice = input("Roll or Hold?\n--> ")
 
-                if choice.lower() == "roll":
+                if(choice.lower() == "roll"):
                     roll = self.player2.roll_dice()
-                    if roll == 1:
+                    if(roll == 1):
                         print(f"{self.player2.get_name()} rolled a {roll} and loses all points for this turn!")
                         round_score = 0
                         break
                     else:
                         round_score += roll
-                        print(f"{self.player2.get_name()} rolled a {roll} and now has {round_score} points!")
+                        print(f"{self.player2.get_name()} rolled a {roll} and now has {self.player2.get_score() + round_score} points!")
 
-                elif choice.lower() == "hold":
+                elif(choice.lower() == "hold"):
                     self.player2.add_score(round_score)
                     print(f"{self.player2.get_name()} has secured {self.player2.get_score()} points!")
                     round_score = 0
                     break
+
+                # Check score
+            if(self.player2.get_score() >= 20):
+                print(f"{self.player2.get_name()} wins with a score of {self.player2.get_score()}!")
+                print("Game ended!")
+                break
                     
 
                 
