ca18fb0a18df8236eb265dfe710ba714336a7002
Isaac Andersson <isaac.andersson0015@stud.hkr.se>
2024-04-17 11:51:03 +0200

    Added functionality for two players

--- game.py
+++ game.py
@@ -4,7 +4,6 @@ import cmd
 import random
 
 class Game():
-    
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
@@ -13,31 +12,60 @@ class Game():
         print("The game has started!")
 
         # Main loop
+        keep_going = True
         while self.player1.get_score() < 20 or self.player2.get_score() < 20:
             
-            keep_going = True
             round_score = 0
-
+            print("NEW ROUND")
+            # Player 1 turn
+            print(f"\n--- {self.player1.get_name()}'s turn ---")
             while keep_going:
-                roll = self.player1.roll_dice()
-                print(f"{self.player1.get_name()} rolled a {roll}!")
-                # If a player rolls 1, the score for this round is lost
-                if(roll == 1):
-                    round_score = 0
-                    print(f"{self.player1.get_name()} loses all the points for this run!")
-                    keep_going = False
-                    break
-                # Otherwise, the score is added to its total
-                else:
-                    round_score += roll
-                    print(f"{self.player1.get_name()} now has {round_score} points!")
-                # Ask the player to hold or roll again
-                choice = input("Roll or Hold?")
+                
+                
+                # Ask the player to roll or hold
+                choice = input("Roll or Hold?\n--> ")
+
                 if choice.lower() == "roll":
-                    keep_going = True
+                    roll = self.player1.roll_dice()
+                    if roll == 1:
+                        print(f"{self.player1.get_name()} rolled a {roll} and loses all points for this turn!")
+                        round_score = 0
+                        break
+                    else:
+                        round_score += roll
+                        print(f"{self.player1.get_name()} rolled a {roll} and now has {round_score} points!")
+
                 elif choice.lower() == "hold":
                     self.player1.add_score(round_score)
-                    keep_going = False
+                    print(f"{self.player1.get_name()} has secured {self.player1.get_score()} points!")
+                    round_score = 0
+                    break
+
+            print(f"\n--- {self.player2.get_name()}'s turn ---")
+            while keep_going:
+                # Player 2's turn
+                
+                # Ask the player to roll or hold
+                choice = input("Roll or Hold?\n--> ")
+
+                if choice.lower() == "roll":
+                    roll = self.player2.roll_dice()
+                    if roll == 1:
+                        print(f"{self.player2.get_name()} rolled a {roll} and loses all points for this turn!")
+                        round_score = 0
+                        break
+                    else:
+                        round_score += roll
+                        print(f"{self.player2.get_name()} rolled a {roll} and now has {round_score} points!")
+
+                elif choice.lower() == "hold":
+                    self.player2.add_score(round_score)
+                    print(f"{self.player2.get_name()} has secured {self.player2.get_score()} points!")
+                    round_score = 0
+                    break
+                    
+
+                
                     
 
 
