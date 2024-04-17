ce532b7a68acf828a78af913377a6aa9475e1e65
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-17 10:41:29 +0200

    changed in constructor

--- dice.py
+++ dice.py
@@ -2,10 +2,10 @@ import random
 
 """Class representing a die that the player and computer controlled player can roll"""
 class Dice:
-    def __init__(self):
+    def __init__(self, low, high):
         """Creates a dice class with low number 1 and high number 6, representing a 6-faced die"""
-        self.low = 1
-        self.high = 6
+        self.low = low
+        self.high = high
 
     def roll_dice(self):
         """Rolls the dice for a number between 1 and 6"""
