28b130b834296731f2a0c866fbad61e1317aa060
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-04-19 12:27:04 +0200

    Removed trash and fixed menu

--- main.py
+++ main.py
@@ -5,46 +5,29 @@ from game import Game
 import computer
 
 class GameLoop(cmd.Cmd):
-    intro = "=== Pig Dice Game ===\n1] Player VS Computer - pvc\n2] Player VS Player - pvp\n3] High score\n4]\n5]"
+    intro = "=== Pig Dice Game ===\n1] Player VS Computer - pvc\n2] Player VS Player - pvp\n3] High score\n4] Quit program - quit\n5]"
     prompt = "--> "
     
     def do_quit(self, line):
-
         """Quits the program"""
         return True
     
     def do_menu(self, arg):
         """Shows game menu"""
-
-        print("\n1] Player VS Computer - PVC\n2] Player VS Player - PVP\n3] High score\n4]\n5]")
+        print("\n1] Player VS Computer - pvc\n2] Player VS Player - pvp\n3] High score\n4] Quit program - quit\n5]")
 
     def do_pvc(self, arg):
         """Selects the game mode to play against the computer"""
-        
-        '''name = input("Enter name for player: ")
-        player1 = player.Player(name)
-        print("choose what difficulty \n1] Normal\n2] Hard\n3] Extreme (you won't win)\n")
-        difficulty = int(input("--> "))
-        diff = difficulty * 6
-        computer1 = computer.Computer(1, diff)
-        pvc_game = PlayerVsComputer(player1, computer1)
-        pvc_game.start()'''
         Game().Player_Computer()
+        self.do_menu(arg)
 
 
 
 
     def do_pvp(self, arg):
-        """Selects the game mode for two players"""
-        '''print("Starting a player versus player game!")
-        name = input("Enter name of player 1: ")
-        player1 = player.Player(name)
-        name = input("Enter name of player 2: ")
-        player2 = player.Player(name)
-        pvp_game = Game(player1, player2)
-        pvp_game.start()'''
-
+        """Selectes gamemode Player vs Player"""
         Game().Player_v_Player()
+        self.do_menu(arg)
 
     def do_highscore(self, arg):
         """Show the high score of previous games by players"""
