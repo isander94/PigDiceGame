216b3f5ccba711183a34dae127de55fafc0834cb
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-05-01 16:42:23 +0200

    added rules

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ class GameLoop(cmd.Cmd):
               "|------------------|-----------|\n" +
               "|     Highscore    | highscore |\n" +
               "|------------------|-----------|\n" +
+              "|      Rules       |   rules   |\n" +
+              "|------------------|-----------|\n" +
               "|   Quit program   |   quit    |\n" +
               " ------------------------------")
 
@@ -39,6 +41,23 @@ class GameLoop(cmd.Cmd):
         high = highscore.Highscore()
         high.show_highscore()
         self.do_menu(arg)
+
+    def do_rules(self, arg):
+        """Displays the rules of the game"""
+        print(
+            """\n
+              Each turn a player gets to roll their dice as
+              many times as they want, accumulating all points they get
+              along the way. However if you roll a 1, you lose all your
+              points you have gathered on that turn. Therefore it could be
+              wise to hold on to your points after a few rolls.
+
+              After the first player has either rolled a 1, or has decided to
+              stay with their points, the turn goes over to the next player.
+
+              This goes on until someone reaches 100 points first
+
+              Good luck!""")
 
 
 if __name__ == '__main__':
