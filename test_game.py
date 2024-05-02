73d8120bc7d834f08b9afc822193883a443bb1c0
Leon Kardum <leon.kardum0040@stud.hkr.se>
2024-05-02 13:58:04 +0200

    added new test to game

--- test_game.py
+++ test_game.py
@@ -12,3 +12,10 @@ class testGameClass(unittest.TestCase):
         will return False if it works properply"""
         g = self.g.check_if_win(20, "galileo")
         self.assertFalse(g)
+
+    def test_checkNotWinner(self):
+        """Same method check but this time
+        checks that the player hasn't won, will return None
+        if it works properly"""
+        g = self.g.check_if_win(15, "galileo")
+        self.assertIsNone(g)
